08c423a69103fbc00c3b91120d571963f4d5ef98
maks <mamonov.maksim.20003@gmail.com>
2022-08-17 20:34:10 +0500

    Четвёртое задание выполнено

--- src/Main.java
+++ src/Main.java
@@ -19,13 +19,26 @@ public class Main {
         System.out.println("Сумма трат за месяц составила " + arrOne + " рублей");
         //второе задание
         int arrTwo = 0;
+        int arrThree = 0;
         for (int i = 0; i < arr.length - 1; i++) {
             if (arr[i] < arr[i + 1]) {
                 arrTwo = arrTwo + arr[i];
 
             }
+            if (arr[i] > arr[i + 1]) {
+                arrThree = arrThree + arr[i];
+            }
         }
-        System.out.println(arrTwo);
-        //
+        System.out.println("Минимальная сумма трат за день составила " + arrTwo + " рублей. Максимальная сумма трат за день составила " + arrThree + " рублей");
+        //Третье задание
+        int division = 0;
+        division = arrOne / arr.length - 1;
+        System.out.println("Средняя сумма затрат за месяц составила " + division + " рублей");
+        //Четвёртое задание
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = 29; i < reverseFullName.length; i--) {
+            System.out.print(reverseFullName[i]);
+        }
+
     }
-}
+    }
